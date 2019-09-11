df759be96a79e61168cd44fd7d370070009e6d74
Bailey Beck <baileymbeck@gmail.com>
2019-09-10 22:34:14 -0500

    added instruct.txt file, linked jquery, typed psuedo code into js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,34 @@
 // set game variables from html
-var randomNumber
-var guessNumber
-var wins
-var losses
+var randomNumber = 0;
+var secretNumber = 0;
+var wins = 0;
+var losses = 0;
+
+
+// <---------- Psuedo code ---------->
+
+// may need to reevaluate secretNumber var name
+
+// ----- At start -----
+// per the youtube demo the game loads a random number with a value between 19 - 120 in id = "randomNumber"
+// the image buttons also has generated random numbers with a value between 1 - 12 in each box - id="box-one", id="box-two", etc
+
+//  ----- On click -----
+// when an img btn is clicked, the random number associated with it will appear in id = "secretNumber"
+// the subsequent clicks of other crystals will be added to the secretNumber total, allowing the player to determine to secret numbers
+
+// ----- Determine win/lose -----
+
+// WIN
+// if numbers added to secretNumber === randomNumber
+// wins++
+// randomNumber reset
+// secretNumbers = 0
+// id boxes 1-4 reset
+
+// LOSE
+// losses++
+// randomNumber reset
+// secretNumbers = 0
+// id boxes 1-4 reset
+
